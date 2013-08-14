6c3c894ce7bf70effde318cba3023b4b9d3bff1c
Graham Briggs <graham.e.briggs.tpc@gmail.com>
2013-08-13 21:25:15 -0700

    Creating project

---
+++ AndroidJava/TCPConnect/src/com/littlebytesofpi/tcpconnect/TCPConnectService.java
@@ -0,0 +1,19 @@
+package com.littlebytesofpi.tcpconnect;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TCPConnectService extends Service {
+
+	public TCPConnectService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
